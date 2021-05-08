e1f8ecab5f9c9fcc8d6457f5cb86546cfd3a8010
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-05-08 15:36:03 -0300

    criando organismo header

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
-<div class="app">
-  <SocialNetworks />
-</div>
+  <div class="app">
+   <Header />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import { SocialNetworks } from '@/components/molecules'
+import { defineComponent } from 'vue';
+import { Header } from '@/components/organisms';
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    SocialNetworks
-  }
+    Header,
+  },
 });
 </script>
 
 <style lang="scss" scoped>
   .app {
-    width: 150px;
+    padding: 2rem;
   }
 </style>
