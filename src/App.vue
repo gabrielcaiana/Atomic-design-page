4814d78f8708127b44e4f968bcb36ead3facaece
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-05-08 15:59:13 -0300

    criando organismo de services

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,20 @@
   <div class="app">
    <Header />
    <Presentation />
+   <Services />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { Header, Presentation } from '@/components/organisms';
+import { Header, Presentation, Services } from '@/components/organisms';
 
 export default defineComponent({
   name: 'App',
   components: {
     Header,
-    Presentation
+    Presentation,
+    Services
   },
 });
 </script>
---
+++ src/components/organisms/Services.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="services">
+    <Service
+      title="Front end"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat aliquam eligendi velit libero assumenda. Velit aliquam laboriosam esse modi repellat, sit ipsum laudantium porro repudiandae sed cumque architecto inventore! Deserunt!"
+    />
+
+    <Service
+      title="Back end"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat aliquam eligendi velit libero assumenda. Velit aliquam laboriosam esse modi repellat, sit ipsum laudantium porro repudiandae sed cumque architecto inventore! Deserunt!"
+    />
+
+    <Service
+      title="Manutenção"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat aliquam eligendi velit libero assumenda. Velit aliquam laboriosam esse modi repellat, sit ipsum laudantium porro repudiandae sed cumque architecto inventore! Deserunt!"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Service } from '@/components/molecules';
+
+export default defineComponent({
+  components: {
+    Service,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.services {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 5rem;
+}
+</style>
