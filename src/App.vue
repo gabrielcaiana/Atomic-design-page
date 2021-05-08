937a9b3758eeabbc31267403f2fd3b4737edb457
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-05-08 15:53:05 -0300

    criando organismo de apresentacao

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="app">
    <Header />
+   <Presentation />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { Header } from '@/components/organisms';
+import { Header, Presentation } from '@/components/organisms';
 
 export default defineComponent({
   name: 'App',
   components: {
     Header,
+    Presentation
   },
 });
 </script>
